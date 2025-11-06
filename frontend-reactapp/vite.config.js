6bd6c73b3e9c55baabfa3f7058032b4183b1b4bd
SAMMYB7 <2300040118@kluniversity.in>
2025-11-06 16:19:02 +0530

    Project V1.0

--- frontend-reactapp/vite.config.js
+++ frontend-reactapp/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/2300040118-frontend/',
+  plugins: [react()]
 })
